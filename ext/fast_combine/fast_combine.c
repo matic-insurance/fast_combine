aa67e896c96d289c96c2f0b64d15b8c6615b7c40
Sergey Kukunin <sergey.kukunin@gmail.com>
2017-02-24 15:06:54 +0200

    Add dummy singleton method to the module

--- ext/fast_combine/fast_combine.c
+++ ext/fast_combine/fast_combine.c
@@ -2,8 +2,14 @@
 
 VALUE rb_mFastCombine;
 
+static VALUE method_combine(VALUE self, VALUE array, VALUE mappings)
+{
+  return rb_ary_new();
+}
+
 void
 Init_fast_combine(void)
 {
   rb_mFastCombine = rb_define_module("FastCombine");
+  rb_define_singleton_method(rb_mFastCombine, "combine", method_combine, 2);
 }
--- spec/fast_combine_spec.rb
+++ spec/fast_combine_spec.rb
@@ -5,7 +5,61 @@ RSpec.describe FastCombine do
     expect(FastCombine::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.combine' do
+    subject(:result) { described_class.combine(input, mappings) }
+
+    let(:input) { [] }
+    let(:mappings) { [] }
+
+    it { is_expected.to be_a(Array) }
+
+    xdescribe 'integration test' do
+      let(:input) do
+        [
+          [
+            {name: 'Jane', email: 'jane@doe.org'},
+            {name: 'Joe', email: 'joe@doe.org'}
+          ],
+          [
+            [
+              # user tasks
+              [
+                {user: 'Jane', title: 'One'},
+                {user: 'Jane', title: 'Two'},
+                {user: 'Joe', title: 'Three'}
+              ],
+              [
+                # task tags
+                [
+                  {task: 'One', tag: 'red'},
+                  {task: 'Three', tag: 'blue'}
+                ]
+              ]
+            ]
+          ]
+        ]
+      end
+
+      let(:mappings) { [[:tasks, {name: :user}, [[:tags, title: :task]]]] }
+
+      it 'merges hashes from arrays using provided join keys' do
+        output = [
+          {name: 'Jane', email: 'jane@doe.org', tasks: [
+            {user: 'Jane', title: 'One', tags: [{task: 'One', tag: 'red'}]},
+            {user: 'Jane', title: 'Two', tags: []}
+          ]},
+          {
+            name: 'Joe', email: 'joe@doe.org', tasks: [
+              {
+                user: 'Joe', title: 'Three', tags: [
+                  {task: 'Three', tag: 'blue'}
+                ]
+              }
+            ]
+          }
+        ]
+        is_expected.to eql(output)
+      end
+    end
   end
 end
