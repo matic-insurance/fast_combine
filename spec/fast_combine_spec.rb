8bb16987acdce96b54c463f2331980e95b8e8e97
Sergey Kukunin <sergey.kukunin@gmail.com>
2017-02-24 16:47:26 +0200

    Implement map on macros

--- spec/fast_combine_spec.rb
+++ spec/fast_combine_spec.rb
@@ -8,10 +8,55 @@ RSpec.describe FastCombine do
   describe '.combine' do
     subject(:result) { described_class.combine(input, mappings) }
 
-    let(:input) { [] }
+    let(:input) { [[]] }
     let(:mappings) { [] }
 
     it { is_expected.to be_a(Array) }
+
+    it { is_expected.to eq([]) }
+
+    context 'without groups' do
+      let(:input) do
+        [
+          [
+            {name: 'Jane', email: 'jane@doe.org'}.freeze,
+            {name: 'Joe', email: 'joe@doe.org'}.freeze
+          ].freeze
+        ].freeze
+      end
+
+      it { is_expected.to eq input.first }
+    end
+
+    context 'with one group' do
+      let(:input) do
+        [
+          [
+            {name: 'Jane', email: 'jane@doe.org'}.freeze,
+            {name: 'Joe', email: 'joe@doe.org'}.freeze
+          ].freeze,
+          [
+            {user: 'Jane', title: 'One'}.freeze,
+            {user: 'Jane', title: 'Two'}.freeze,
+            {user: 'Joe', title: 'Three'}.freeze
+          ]
+        ].freeze
+      end
+      let(:mappings) { [:tasks, {name: :user}] }
+
+      it 'merges hashes from arrays using provided join keys' do
+        output = [
+          {name: 'Jane', email: 'jane@doe.org', tasks: [
+            {user: 'Jane', title: 'One'},
+            {user: 'Jane', title: 'Two'}
+          ]},
+          {name: 'Joe', email: 'joe@doe.org', tasks: [
+            {user: 'Joe', title: 'Three'}
+          ]}
+        ]
+        is_expected.to eql(output)
+      end
+    end
 
     xdescribe 'integration test' do
       let(:input) do
