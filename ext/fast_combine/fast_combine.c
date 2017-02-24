de0c985c12c1186efdf103d4e54ee342113d4e39
Sergey Kukunin <sergey.kukunin@gmail.com>
2017-02-24 17:14:09 +0200

    Better map skeleton

--- ext/fast_combine/fast_combine.c
+++ ext/fast_combine/fast_combine.c
@@ -1,37 +1,33 @@
 #include "fast_combine.h"
 
-#define FASTCOMBINE_MAP(ary, collect, iterator) do {  \
-  long i; \
-  collect = rb_ary_new2(RARRAY_LEN(ary)); \
-  for (i = 0; i < RARRAY_LEN(ary); i++) { \
-    VALUE element = RARRAY_AREF(ary, i); \
-    VALUE new_element; \
-    iterator; \
-    rb_ary_push(collect, new_element);              \
-  } \
-}while(0)
-
 VALUE rb_mFastCombine;
 
-static VALUE fastcombine_method_combine(VALUE self, VALUE array, VALUE mappings);
+static VALUE method_combine(VALUE self, VALUE array, VALUE mappings);
+static VALUE combine_element(VALUE element, VALUE groups, VALUE mappings);
 
 void
 Init_fast_combine(void)
 {
   rb_mFastCombine = rb_define_module("FastCombine");
-  rb_define_singleton_method(rb_mFastCombine, "combine", fastcombine_method_combine, 2);
+  rb_define_singleton_method(rb_mFastCombine, "combine", method_combine, 2);
 }
 
-static VALUE fastcombine_method_combine(VALUE self, VALUE array, VALUE mappings)
+static VALUE method_combine(VALUE self, VALUE array, VALUE mappings)
 {
   VALUE root = rb_ary_entry(array, 0),
     groups = rb_ary_subseq(array, 1, RARRAY_LEN(array));
 
-  VALUE to_return;
-  FASTCOMBINE_MAP(root, to_return, {
-      VALUE copy = rb_hash_dup(element);
-      rb_hash_aset(copy, rb_str_new2("tasks"), rb_ary_new());
-      new_element = copy;
-  });
-  return to_return;
+  VALUE final_root = rb_ary_new2(RARRAY_LEN(root));
+  for (long i = 0; i < RARRAY_LEN(root); i++) {
+    VALUE element = RARRAY_AREF(root, i);
+    VALUE new_element = combine_element(element, groups, mappings);
+    rb_ary_push(final_root, new_element);
+  }
+  return final_root;
+}
+
+static VALUE combine_element(VALUE element, VALUE groups, VALUE mappings) {
+  VALUE copy = rb_hash_dup(element);
+  rb_hash_aset(copy, rb_str_new2("tasks"), rb_ary_new());
+  return copy;
 }
